e0ca15d9bea3f642925ec5d9219785c5a40acbf5
Ryan Vu <vuryan612@gmail.com>
2025-04-28 02:24:15 -0700

    sum of array macro

--- Macros.asm
+++ Macros.asm
@@ -17,5 +17,19 @@
 	syscall
 .end_macro
 
+.macro sumArray(%array, %size)	#sum saved to $v0
+	la $t2, %array	#address of array
+	li $t3, 0		#counter
+	li $v0, 0		#sum
+	
+loop_sumArray:
+	bge $t3, %size, end_sumArray
+	lw $t4, 0($t2)		#load array element
+	add $v0, $v0, $t4	#sum += element
+	add $t2, $t2, 4		#next element
+	add $t3, $t3, 1		#counter++
+	j loop_sumArray
 
+end_sumArray:	#do nothing, exit out of macro
+.end_macro
 
